2edd04834b2763712251fa8535019988d1d84008
Yauhenii <yauhenmardan@gmail.com>
2023-03-23 08:56:34 +0100

    add dump request

--- back/src/app/api.py
+++ back/src/app/api.py
@@ -1,7 +1,7 @@
 import logging
 from fastapi import FastAPI
 
-from src.app.data_transfer import AnswerRequest, AnswerResponse
+from src.app.data_transfer import AnswerRequest, AnswerResponse, DumpRequest
 
 app = FastAPI()
 logger = logging.getLogger("uvicorn.error")
@@ -10,3 +10,8 @@ logger = logging.getLogger("uvicorn.error")
 @app.post("/request_answer", response_model=AnswerResponse, status_code=200)
 async def post_request_answer(answer_request: AnswerRequest):
     return AnswerResponse(id=f'milena_{answer_request.id}')
+
+
+@app.post("/dump", status_code=200)
+async def post_dump(dump_request: DumpRequest):
+    return {}
--- back/src/app/data_transfer/__init__.py
+++ back/src/app/data_transfer/__init__.py
@@ -1,2 +1,3 @@
 from src.app.data_transfer.AnswerRequest import AnswerRequest
 from src.app.data_transfer.AnswerResponse import AnswerResponse
+from src.app.data_transfer.DumpRequest import DumpRequest
